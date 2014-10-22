5e4553dbd3a307206a6d887d13ae13412fab17b8
Lalo <lalo.sa.94@gmail.com>
2014-10-22 10:44:01 -0500

    Initial Commit

---
+++ Nizer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Nizer
+//
+//  Created by Eduardo Alberto Sanchez Alvarado on 10/22/14.
+//  Copyright (c) 2014 Eduardo Alberto Sanchez Alvarado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Nizer/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  Nizer
+//
+//  Created by Eduardo Alberto Sanchez Alvarado on 10/22/14.
+//  Copyright (c) 2014 Eduardo Alberto Sanchez Alvarado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
