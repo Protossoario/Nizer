0e4de51358e5bccb3199a46e91e8abdb29aa6405
alumno <alumno@ESL-3086.local>
2014-12-01 18:03:51 -0600

    BarGraphViewController

---
+++ Nizer/BarGraphViewController.h
@@ -0,0 +1,13 @@
+//
+//  BarGraphViewController.h
+//  Nizer
+//
+//  Created by alumno on 01/12/14.
+//  Copyright (c) 2014 Eduardo Alberto Sanchez Alvarado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BarGraphViewController : UIViewController
+
+@end
---
+++ Nizer/BarGraphViewController.m
@@ -0,0 +1,49 @@
+//
+//  BarGraphViewController.m
+//  Nizer
+//
+//  Created by alumno on 01/12/14.
+//  Copyright (c) 2014 Eduardo Alberto Sanchez Alvarado. All rights reserved.
+//
+
+#import "BarGraphViewController.h"
+
+@interface BarGraphViewController ()
+
+@end
+
+@implementation BarGraphViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
